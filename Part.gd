295f8a00a66a466b82803a431eed12f2010a509c
Julian Benda <julian.benda@ovgu.de>
2022-10-30 00:01:06 +0200

    Add first IK "punsh"

--- Part.gd
+++ Part.gd
@@ -8,21 +8,24 @@ class_name Part
 var projectileScene = preload("res://Projectile.tscn")
 
 # Called when the node enters the scene tree for the first time.
+var proj = null
 func _ready() -> void:
-	var proj = projectileScene.instance()
-	proj.global_position = global_position + Vector2.RIGHT * 100
-	proj.speed = 500
+	proj = projectileScene.instance()
+	proj.global_position = global_position + Vector2.RIGHT * 64
+	proj.speed = 70
 	get_tree().root.add_child(proj)
-	var tracker = RemoteTransform2D.new()
-	proj.add_child(tracker)
-	tracker.update_position = true
-	tracker.remote_path = $"Body/Skeleton2D/Bone2D/Bone2D/Bone2D".get_path()
-
-	$"Body/Skeleton2D/Bone2D/Bone2D/Bone2D".global_position = Vector2.ZERO
+	# var tracker = RemoteTransform2D.new()
+	# proj.add_child(tracker)
+	# tracker.update_position = true
+	# tracker.remote_path = $"Body/Skeleton2D/Bone2D/Bone2D/Bone2D".get_path()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+func _process(_delta: float) -> void:
+	if proj and is_instance_valid(proj) and not proj.is_queued_for_deletion(): 
+		$"Body/InverseKinematic".reach_toward(proj.global_position)
+		# print(proj.global_position, $Body/Skeleton2D/Bone2D/Bone2D/Position2D.global_position)
 
 func stack(base: NodePath) -> void:
 	$"Spring left".node_a = base
