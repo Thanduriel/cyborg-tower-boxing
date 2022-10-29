1fa2fb9dc7a8b8a60be324f4581eacc07288103a
Julian Benda <julian.benda@ovgu.de>
2022-10-29 19:06:21 +0200

    Added physics movement

--- Legs.gd
+++ Legs.gd
@@ -5,7 +5,8 @@ extends RigidBody2D
 # var a: int = 2
 # var b: String = "text"
 
-export var speed = 500
+export var speed: float = 500
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,10 +14,13 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
-		linear_velocity.x = -speed
-		# move_and_slide(Vector2.LEFT * speed, Vector2.UP)
-	if Input.is_action_pressed("move_right"):
-		linear_velocity.x = speed
-		#move_and_slide(Vector2.RIGHT * speed, Vector2.UP)
-
+	
+	# apply_central_impulse(-linear_velocity.normalized() * damp * delta * linear_velocity.length_squared())
+	
+	for x in get_colliding_bodies():
+		if x.get_name() == "GroundCollider":
+			if Input.is_action_pressed("move_left"):
+				apply_central_impulse(Vector2.LEFT * speed * delta)
+			elif Input.is_action_pressed("move_right"):
+				apply_central_impulse(Vector2.RIGHT * speed * delta)
+			
