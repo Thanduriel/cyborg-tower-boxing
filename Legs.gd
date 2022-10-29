e6205f68b8a5e088be59398dbc780b2e07f699bf
Janek Winkler <janekx21@gmail.com>
2022-10-29 20:48:26 +0200

    joint spawning

--- Legs.gd
+++ Legs.gd
@@ -6,11 +6,22 @@ extends RigidBody2D
 # var b: String = "text"
 
 export var speed: float = 500
+var partScene = preload("res://Part.tscn")
 
+var head = self
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("spawn"):
+		var part = partScene.instance()
+		part.global_position = head.global_position + Vector2.UP.rotated(head.global_rotation) * 70
+		part.global_rotation = head.global_rotation
+		part.stack(head.get_path())
+		get_tree().root.add_child(part)
+		head = part.get_node("Body")
 
 func _physics_process(delta: float) -> void:
 	for x in get_colliding_bodies():
